5070bf810b601cc0b3601271d10364c412d26e75
budianer321 <422665958@qq.com>
2018-10-30 21:09:51 +0800

    登录/注册/退出搭建1

--- App/models.py
+++ App/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=20)
+    psw1 = models.CharField(max_length=256)
+    user_img = models.CharField(max_length=256)
--- App/views.py
+++ App/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 # Create your views here.
-def index(request):
+from App.models import User
 
-    return render(request,'index1.html')
+
+def index(request):
+    # 获取cookie
+    user_name = request.COOKIES.get('user_name')
+
+    return render(request, 'index1.html', context={'user_name': user_name})
 
 
 def cart(request):
@@ -19,8 +25,73 @@ def goods2(request):
 
 
 def login(request):
-    return render(request,'login.html')
+    if request.method == 'GET':  # 获取登录页面
+        return render(request, 'login.html')
+    elif request.method == 'POST':  # 登录操作
+        # 获取数据
+        user_name = request.POST.get('user_name')
+        psw1 = request.POST.get('psw1')
+        print(user_name, psw1)
+
+        # 验证
+        # 数据库能找到，登录成功
+        # 数据库找不到，登录失败
+        users = User.objects.filter(user_name=user_name).filter(psw1=psw1)
+        if users.count():  # 存在
+
+            user = users.last()
+            print(user.user_name)
+        #
+            # 重定向首页
+            response = redirect('App:index')
+
+            # 设置cookie
+            response.set_cookie('user_name', user.user_name)
+
+            return response
+            # return HttpResponse('登录成功')
+        else:  # 不存在
+            return HttpResponse('用户名或密码错误!')
+
+
+    # return render(request,'login.html')
 
 
 def register(request):
-    return render(request,'register.html')
+    if request.method == 'GET':  # 获取注册页面
+        return render(request, 'register.html')
+    elif request.method == 'POST':  # 注册操作
+        # 获取客户端传入的数据
+        user_name = request.POST.get('user_name')
+        psw1 = request.POST.get('psw1')
+        psw2 = request.POST.get('psw2')
+        # if psw1 != psw2:
+
+
+        print(user_name, psw1, psw2)
+
+
+        # 存入数据库
+        user = User()
+        user.user_name = user_name
+        user.psw1 = psw1
+        user.save()
+
+        # 重定向首页
+        response = redirect('App:index')
+
+        # 状态保持
+        response.set_cookie('user_name', user_name)
+
+        # return HttpResponse('注册成功')
+        return response
+
+# 退出
+def logout(request):
+    # 重定向首页
+    response = redirect('App:index')
+
+    # 删除cookie
+    response.delete_cookie('user_name')
+
+    return response
